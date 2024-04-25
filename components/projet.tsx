f56c7f14b1bfe2996b5154b765ad6fffc09593c2
Lionel Auray <lionel.auray@gmail.com>
2024-04-25 16:29:58 +0200

    blur image projet

--- components/projet.tsx
+++ components/projet.tsx
@@ -44,7 +44,9 @@ export default function Project({
         src={imageUrl}
         alt="Les projets sur lesquels j'ai travaillé"
         quality={95}
-        className="blur absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:-translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
+        className={`${
+          title !== "Clone de Netflix" ? "blur" : ""
+        } absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:-translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40`}
       />
     </motion.section>
   );
